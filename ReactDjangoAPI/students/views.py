7165eb1644427efd508d241995c12d19d32df1e9
Aryan Santosh Sawant <102072945+LaxmansAryan@users.noreply.github.com>
2023-03-26 16:36:39 +0530

    Add files via upload

---
+++ ReactDjangoAPI/students/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
+from .models import Student
+from .serializers import *
+
+# Create your views here.
+@api_view(['GET', 'POST'])
+def students_list(request):
+    if request.method == 'GET':
+        data = Student.objects.all()
+
+        serializer = StudentSerializer(data, context={'request': request}, many=True)
+
+        return Response(serializer.data)
+    
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PUT', 'DELETE'])
+def student_detail(request, pk):
+    try:
+        student = Student.objects.get(pk=pk)
+    except Student.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'PUT':
+        serializer = StudentSerializer(student, data=request.data,context={'request': request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        student.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
